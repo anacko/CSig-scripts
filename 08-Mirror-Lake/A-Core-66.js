8e184b6ab072cd35aa5470aac491af7ef6440da9
Ana Carolina K <anacko@gmail.com>
2022-03-17 16:43:34 -0400

    Add 66 of The Core

---
+++ 08-Mirror-Lake/A-Core-66.js
@@ -0,0 +1,15 @@
+function solution(divisors, k) {
+    
+  const clans = [];
+  
+  for(let i = 1; i <= k; i++) {
+      // gives a name to the clan: string representing if each the divisor is a divisor of i or not
+      const clanName = divisors.map(e => i % e ? '1' : '0').join('')
+      clans.includes(clanName) ? null : clans.push(clanName)
+  }
+  
+  return clans.length
+
+}
+
+console.log([2, 3], 6) // 4
